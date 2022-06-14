ad5a8114af8cf016a8121ccb4707e26c343818eb
Khuongduyman <91811880+Khuongduyman@users.noreply.github.com>
2022-06-14 07:13:41 -0700

    Bài tập vòng lập

---
+++ main5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int count,min,max;
+printf(" nhap min: ");
+scanf("%d", &min);
+
+printf(" nhap max: ");
+scanf("%d", &max);
+
+
+for ( count=min; count<=max ; count++) {
+if ( count%2==0) {
+printf("\n %d",count);
+
+					}
+
+
+									}				
+
+		return 0;
+}
